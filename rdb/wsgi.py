1953a4c1141de710c10f723ca47cfe5659a1241b
Danny Mota <dannymota@Dannys-MacBook-Pro.local>
2021-01-28 12:43:20 -0500

    Add white noise to fix static files issue

--- rdb/wsgi.py
+++ rdb/wsgi.py
@@ -7,10 +7,18 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rdb.settings')
+
+# application = get_wsgi_application()
+
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rdb.settings')
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rdb.settings")
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
