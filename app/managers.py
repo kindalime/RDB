5969dee3bb1a6ef480d58e6d6065083ea66d316b
Danny Mota <dannymota@Dannys-MacBook-Pro.local>
2021-02-12 02:00:00 -0500

    Update lab search manager

--- app/managers.py
+++ app/managers.py
@@ -3,26 +3,33 @@ from django.contrib.postgres.search import (
     SearchQuery, SearchRank, SearchVector, TrigramSimilarity,
 )
 from django.db import models
+from django.db.models.functions import Greatest
 
 
 class LabManager(models.Manager):
     def search(self, search_text):
         search_vectors = (
-            SearchVector(
-                'project_desc', weight='A'
-            )
-        ) 
-        search_query = SearchQuery(
-            search_text
+            SearchVector('name', weight='A') 
+            + SearchVector('project_desc', weight='B')
+            + SearchVector('pi_name', weight='C')
+            + SearchVector('email', weight='D')
         )
+
+        search_query = SearchQuery(search_text)
+
         search_rank = SearchRank(search_vectors, search_query)
-        trigram_similarity = TrigramSimilarity(
-            'project_desc', search_text
+        trigram_similarity = Greatest(
+            TrigramSimilarity('name', search_text),
+            TrigramSimilarity('project_desc', search_text),
+            TrigramSimilarity('pi_name', search_text),
+            TrigramSimilarity('email', search_text),
         )
+        search_rank += trigram_similarity
+
         qs = (
             self.get_queryset()
             .filter(search_vector=search_query)
-            .annotate(rank=search_rank + trigram_similarity)
+            .annotate(rank=search_rank)
             .order_by('-rank')
         )
         return qs
