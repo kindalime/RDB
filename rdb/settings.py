ad9c4f54da2f84d99e5430aef68a4706e2d91538
Danny Mota <dannymota@Dannys-MBP.fios-router.home>
2021-03-06 21:03:33 -0500

    Remove secrets paramter so heroku doesn't throw error

--- rdb/settings.py
+++ rdb/settings.py
@@ -25,7 +25,7 @@ if DEBUG:
     with open(os.path.join(BASE_DIR, 'secrets.json')) as secrets_file:
         secrets = json.load(secrets_file)
 
-def get_secret(setting, secrets=secrets):
+def get_secret(setting):
     """Get secret setting or fail with ImproperlyConfigured"""
     try:
         if DEBUG:
@@ -44,6 +44,7 @@ SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = get_secret('SECRET_KEY')
 
 ALLOWED_HOSTS = [
