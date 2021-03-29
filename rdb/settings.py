e52521b0b26093b2e2e3d4d15fab05b46d5061c7
Danny Mota <dannymota@Dannys-MacBook-Pro.local>
2021-03-29 11:21:54 -0400

    Bump gzip up

--- rdb/settings.py
+++ rdb/settings.py
@@ -71,9 +71,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django.middleware.gzip.GZipMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.middleware.gzip.GZipMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
