a7b50902dccd2a6b606cb29c5a30e549e4c06c15
Danny Mota <dannymota@Dannys-MBP.fios-router.home>
2021-04-21 23:55:41 -0400

    Patch security flaw

--- app/serializers.py
+++ app/serializers.py
@@ -4,4 +4,7 @@ from .models import Lab
 class LabSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lab
-        fields = '__all__'
+        fields = ('name', 'pi_name', 'department', 'work_remote', 
+                  'work_in_person', 'accept_undergrads', 'accept_grads', 
+                  'email', 'website', 'mentors', 'funded', 'project_desc', 
+                  'created_date', 'modified_date', 'slug', 'likes')
